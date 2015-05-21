d2859c3d553acb54defc1e0f9373d0e2e91e3adf
Patricia <psdr1994@hotmail.com>
2015-05-21 11:14:23 +0100

    meh

---
+++ Desenvolvimento/FINAL/AritMat/AritMat/BOL/Imagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace AritMat.BOL
+{
+    public class Imagem
+    {
+        private Image Imagem;
+    }
+}
---
+++ Desenvolvimento/FINAL/AritMat/AritMat/BOL/Video.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AritMat.BOL
+{
+    public class Video
+    {
+        private string Video;
+    }
+}
