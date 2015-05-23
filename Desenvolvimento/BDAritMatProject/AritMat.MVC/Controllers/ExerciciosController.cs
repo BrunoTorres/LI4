fcae9bd97d541e9ddd51c91550c279988ada8581
Patricia <psdr1994@hotmail.com>
2015-05-23 17:45:23 +0100

    licao,teste,exercicio

---
+++ Desenvolvimento/BDAritMatProject/AritMat.MVC/Controllers/ExerciciosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AritMat.MVC.Models;
+
+namespace AritMat.MVC.Controllers
+{
+    public class ExerciciosController : Controller
+    {
+        private BDAritMatProjectEntities db = new BDAritMatProjectEntities();
+
+        // GET: Exercicios
+        public ActionResult Index()
+        {
+            var exercicios = db.Exercicios.Include(e => e.Administrador1).Include(e => e.Tipo1);
+            return View(exercicios.ToList());
+        }
+
+        // GET: Exercicios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercicio exercicio = db.Exercicios.Find(id);
+            if (exercicio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exercicio);
+        }
+
+        // GET: Exercicios/Create
+        public ActionResult Create()
+        {
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username");
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area");
+            return View();
+        }
+
+        // POST: Exercicios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdExercicio,Tipo,Administrador,Texto,Dificuldade,Imagem,TempoEx")] Exercicio exercicio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exercicios.Add(exercicio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", exercicio.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", exercicio.Tipo);
+            return View(exercicio);
+        }
+
+        // GET: Exercicios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercicio exercicio = db.Exercicios.Find(id);
+            if (exercicio == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", exercicio.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", exercicio.Tipo);
+            return View(exercicio);
+        }
+
+        // POST: Exercicios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdExercicio,Tipo,Administrador,Texto,Dificuldade,Imagem,TempoEx")] Exercicio exercicio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exercicio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", exercicio.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", exercicio.Tipo);
+            return View(exercicio);
+        }
+
+        // GET: Exercicios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exercicio exercicio = db.Exercicios.Find(id);
+            if (exercicio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exercicio);
+        }
+
+        // POST: Exercicios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Exercicio exercicio = db.Exercicios.Find(id);
+            db.Exercicios.Remove(exercicio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Desenvolvimento/BDAritMatProject/AritMat.MVC/Controllers/LicoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AritMat.MVC.Models;
+
+namespace AritMat.MVC.Controllers
+{
+    public class LicoesController : Controller
+    {
+        private BDAritMatProjectEntities db = new BDAritMatProjectEntities();
+
+        // GET: Licoes
+        public ActionResult Index()
+        {
+            var licoes = db.Licoes.Include(l => l.Administrador1).Include(l => l.Tipo1);
+            return View(licoes.ToList());
+        }
+
+        // GET: Licoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Licao licao = db.Licoes.Find(id);
+            if (licao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(licao);
+        }
+
+        // GET: Licoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username");
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area");
+            return View();
+        }
+
+        // POST: Licoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idLicao,NumExpl,Tipo,Administrador,Texto,Video,Imagem,TempoLicao")] Licao licao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Licoes.Add(licao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", licao.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", licao.Tipo);
+            return View(licao);
+        }
+
+        // GET: Licoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Licao licao = db.Licoes.Find(id);
+            if (licao == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", licao.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", licao.Tipo);
+            return View(licao);
+        }
+
+        // POST: Licoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idLicao,NumExpl,Tipo,Administrador,Texto,Video,Imagem,TempoLicao")] Licao licao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(licao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Administrador = new SelectList(db.administradores, "IdAdministrador", "Username", licao.Administrador);
+            ViewBag.Tipo = new SelectList(db.Tipos, "IdTipo", "Area", licao.Tipo);
+            return View(licao);
+        }
+
+        // GET: Licoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Licao licao = db.Licoes.Find(id);
+            if (licao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(licao);
+        }
+
+        // POST: Licoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Licao licao = db.Licoes.Find(id);
+            db.Licoes.Remove(licao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
